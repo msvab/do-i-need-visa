2ff0f803e7494977306bd7bd82fd306129459dd4
Michal Svab <cbenneh@gmail.com>
2014-03-16 21:40:49 +0000

    Added source validation

--- app/models/visa_source.rb
+++ app/models/visa_source.rb
@@ -1,3 +1,16 @@
 class VisaSource < ActiveRecord::Base
   has_many :visas, dependent: :delete_all
+
+  validates :name, :country, :url, :description, presence: true
+
+  validates :name, :url, :etag, length: { maximum: 255 }
+  validates :country, length: { is: 2 }
+
+  validate :etag_or_date_presence
+
+  private
+
+  def etag_or_date_presence
+    errors.add(:base, 'Etag or last modified date must be set.') if etag.blank? && last_modified.blank?
+  end
 end
