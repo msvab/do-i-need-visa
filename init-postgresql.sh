7bf83b87b5b1357f90676d80e001803bed51f27a
Michal Svab <michal.svab@gamesys.co.uk>
2014-07-24 14:50:30 +0100

    init postgres now actually works

--- init-postgresql.sh
+++ init-postgresql.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
-createdb visa;
-createuser visa;
-createdb visatest;
-createuser visatest;
-# run GRANT ALL PRIVILEGES ON DATABASE visa to visa; in psql
+# delete DB and users if exists
+dropdb visa
+dropdb visatest
+
+# create new DB and users
+createdb visa
+createdb visatest
+
+createuser visa
+createuser visatest
+
+# set up privileges
+psql postgres -c "GRANT ALL PRIVILEGES ON DATABASE visa to visa;"
+psql postgres -c "GRANT ALL PRIVILEGES ON DATABASE visatest to visatest;"
+psql postgres -c "ALTER ROLE visa WITH CREATEDB;"
+psql postgres -c "ALTER ROLE visatest WITH CREATEDB;"
+
+# create schema
+rake db:create:all
