07e60c3fa5282ca1902dc60fdb73e3d8f5f24b53
Michal Svab <cbenneh@gmail.com>
2014-07-27 16:07:23 +0100

    more tests

--- spec/controllers/visa_sources_controller_spec.rb
+++ spec/controllers/visa_sources_controller_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe VisaSourcesController, :type => :controller do
 
   describe 'GET #edit' do
     it 'requires sign-in' do
-      get :edit, id: -1
+      get :edit, id: 1
 
       expect(response).to redirect_to :sign_in
     end
@@ -62,6 +62,57 @@ RSpec.describe VisaSourcesController, :type => :controller do
 
       expect(VisaSource.count).to be 0
       expect(response).to render_template :new
+      expect(flash[:errors]).to include :country
+    end
+  end
+
+  describe 'POST #update' do
+    it 'requires sign-in' do
+      post :update, id: 1, visa_source: FactoryGirl.build(:visa_source)
+
+      expect(response).to redirect_to :sign_in
+    end
+
+    it 'redirects to index page upon successful VisaSource update' do
+      visa_codes = ['US']
+      visa_source = FactoryGirl.create(:visa_source)
+      updated_source = visa_source.attributes.merge({description: 'Hello!', visa_codes: visa_codes})
+
+      sign_in
+      post :update, id: visa_source.id, visa_source: updated_source
+
+      expect(VisaSource.count).to be 1
+      expect(VisaSource.all.first.country).to eq visa_source.country
+      expect(VisaSource.all.first.description).to eq updated_source[:description]
+      expect(VisaSource.all.first.visa_codes).to eq visa_codes
+      expect(response).to redirect_to action: :index
+    end
+
+    it 'updates countries for which the visa sources applies' do
+      visa_codes = ['US', 'GB']
+      visa_source = FactoryGirl.create(:visa_source, visa_codes: ['GB', 'CZ'])
+      updated_source = visa_source.attributes.merge({description: 'Hello!', visa_codes: visa_codes})
+
+      sign_in
+      post :update, id: visa_source.id, visa_source: updated_source
+
+      expect(VisaSource.count).to be 1
+      expect(VisaSource.all.first.country).to eq visa_source.country
+      expect(VisaSource.all.first.description).to eq updated_source[:description]
+      expect(VisaSource.all.first.visa_codes).to contain_exactly *visa_codes
+      expect(response).to redirect_to action: :index
+    end
+
+    it 'displays validation errors upon submitting invalid VisaSource' do
+      visa_source = FactoryGirl.create(:visa_source)
+      updated_source = visa_source.attributes.merge({country: 'rubbish'})
+
+      sign_in
+      post :update, id: visa_source.id, visa_source: updated_source
+
+      expect(VisaSource.count).to be 1
+      expect(VisaSource.all.first.country).to eq visa_source.country
+      expect(response).to render_template :edit
       expect(flash[:errors]).to include :country
     end
   end
